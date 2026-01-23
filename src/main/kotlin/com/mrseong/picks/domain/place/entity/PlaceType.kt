2e1cdff79694c36e01f1f6ad28c1a450b3b29b74
tjddn88 <76651042+tjddn88@users.noreply.github.com>
2026-01-23 18:25:43 +0900

    feat: PlaceType 확장 - 맛집/아이놀이터/아빠의쉼터
    - ATTRACTION → KIDS_PLAYGROUND, RELAXATION으로 변경
    - 맛집, 아이 놀이터, 아빠의 쉼터 3가지 타입 지원

--- src/main/kotlin/com/mrseong/picks/domain/place/entity/PlaceType.kt
+++ src/main/kotlin/com/mrseong/picks/domain/place/entity/PlaceType.kt
@@ -1,6 +1,7 @@
 package com.mrseong.picks.domain.place.entity
 
 enum class PlaceType {
-    RESTAURANT,
-    ATTRACTION
+    RESTAURANT,      // 맛집
+    KIDS_PLAYGROUND, // 아이 놀이터
+    RELAXATION       // 아빠의 쉼터
 }
